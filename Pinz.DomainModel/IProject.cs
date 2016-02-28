39045ec0957158a7eddac3fd21ba367e5e7763c3
Miroslav Furda <miroslav.furda@senacor.com>
2016-02-28 22:32:45 +0100

    nuspec

---
+++ Pinz.DomainModel/IProject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace com.Pinzonline.DomainModel
+{
+    public interface IProject
+    {
+        Guid ProjectId { get; set; }
+        string Name { get; set; }
+        string Description { get; set; }
+        List<ICategory> Categories { get; set; }
+    }
+}
